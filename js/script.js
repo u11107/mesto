6801eaf1afb8810e732e62abde2497bbdd9092d8
Rinat <ren84@inbox.ru>
2021-09-28 20:24:11 +0300

    28.09

--- js/script.js
+++ js/script.js
@@ -7,9 +7,15 @@ const profilSubtitle = document.querySelector('.profile__info-subtitle');
 
 const saveBtn = popup.querySelector('.popup__btn_action_submit');
 
+const popupAdd = document.querySelector('.popup__add_card');// второй модальное окно
+const openPopupEditBtn = document.querySelector('.profile__abb-button');
+const closePopupEdit = popupAdd.querySelector('.popup__btn_action_close');
 
+const foto = document.querySelector('.foto');// like
+const blackLike = foto.querySelector('.foto__like');
+const btnFotoLike = foto.querySelector('.foto__like-black');
 
-let formElement = popup.querySelector('.popup__form');
+let formElement = popup.querySelector('.popup__form');// поля ввода для первого модального она
 let nameInput = popup.querySelector('.popup__item_type_name');
 let jobInput = popup.querySelector('.popup__item_type_vocation');
 
@@ -17,10 +23,25 @@ function popupToggle() {
     popup.classList.toggle('popup_open');
 }
 
+function popupEditToggle() {
+    popupAdd.classList.toggle('popup_open');
+}
+
+function likeToggle() {
+    blackLike.classList.toggle('foto__like-black')
+}
+
+
 function setPopupInoutValue() {
     nameInput.value = profilInfo.textContent
     jobInput.value = profilSubtitle.textContent
     popupToggle();
+
+}
+
+function setPopupAdd() {
+    nameInput.value = profilInfo.textContent
+    jobInput.value = profilSubtitle.textContent
 }
 
 formElement.addEventListener('submit', formSubmitHandler); 
@@ -34,12 +55,17 @@ function formSubmitHandler (evt) {
     evt.preventDefault(); 
     savePopupValue();
     popupToggle();
+    
 }
 
-closePopup.addEventListener('click', popupToggle);
+closePopup.addEventListener('click', popupToggle);// открытие и закрытие первого модально окна
 openPopup.addEventListener('click', setPopupInoutValue);
 
 
+openPopupEditBtn.addEventListener('click', popupEditToggle);// откыртие и закрытие второго модального окна
+closePopupEdit.addEventListener('click', popupEditToggle);
+
+btnFotoLike.addEventListener('click', likeToggle);// слушатель кнопки лайк
 
 
 
